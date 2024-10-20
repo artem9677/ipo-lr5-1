1c5698ede4dd9c5f07b14f1f5f1869fd8a414943
Artem <69763892+artem9677@users.noreply.github.com>
2024-10-20 17:06:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-string = str(input("Введите строку: "))
-print("Длина строки:",len(string))
+string = str(input("Введите строку: ")) # ввод строки
+print("Длина строки:",len(string)) # вывод длины строки
 
-glasn = 0
+glasn = 0 # создание переменных для подсчета гласных и согласных букв
 soglasn = 0
 
-for i in string:
-    if i == "А" or i == "У" or i == "О"or i == "И"or i == "Э"or i == "Ы"or i == "Я"or i == "Ю" or i == "Е" or i == "Ё" or i == "а" or i == "у" or i == "о"or i == "и"or i == "э"or i == "ы"or i == "я"or i == "ю" or i == "е" or i == "ё":
+string = string.lower() # приведение строки к нижнему регистру 
+
+gl = ["а","у","о","и","э","е","ё","я","ю"] # список с гласными буквами
+
+for i in string: # проверка букв
+    if i in gl:
         glasn+=1
-    elif i ==" ":
+    elif i ==" " or i=="!" or i=="?" or i==".":
         continue
     else:
         soglasn+=1
         
 print("Количество гласных: ",glasn)
 print("Количество согласных: ",soglasn)
-
